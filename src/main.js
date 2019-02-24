a959f52b1b160ba89b076d1d8cb24f96324d9a43
Tarek Meftah <sudtechnics@gmail.com>
2019-02-24 20:41:34 +0100

    set the central store vuex

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from "vue";
 import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router/router";
-// import store from "./store/store";
+import store from "./store/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
   el: "#app",
   router,
-  // store,
+  store,
   render: h => h(App)
 });
